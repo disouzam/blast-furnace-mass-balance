54e2372975aa1d7da17aac37ad15bb31cb6e7746
Dickson Souza <disouzam@yahoo.com.br>
2022-10-02 15:08:40 -0300

    Initial console application - boiler plate code targeting .NET 7.0 RC and using old-school Program class

---
+++ src/csharp/blast-furnace-mass-balance-app/Program.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace blast_furnace_mass_balance_app;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Blast Furnace Mass Balance Application");
+    }
+}
