d3c2deabeb8972fc7a7828b8291f9a2ba3790592
Dickson Souza <disouzam@yahoo.com.br>
2022-12-23 19:53:53 -0300

    Implemented conversion of weight units and added tests to check expected behavior

---
+++ src/csharp/BlastFurnace.MassBalance.Lib.Tests/UnitConversionTests.cs
@@ -0,0 +1,21 @@
+﻿using FluentAssertions;
+
+using Xunit;
+
+namespace BlastFurnace.MassBalance.Lib.Tests;
+
+public class UnitConversionTests
+{
+    [Theory]
+    [InlineData(1, WeightUnits.kilogram, 0.001, WeightUnits.metricTon)]
+    [InlineData(1, WeightUnits.metricTon, 999, WeightUnits.kilogram)]
+    public void CheckValidConversions(double originalValue, WeightUnits originalWeightUnit, double convertedValue, WeightUnits newWeightUnit)
+    {
+        var originalWeight = new Weight(originalValue, originalWeightUnit);
+
+        var convertedWeight = UnitConversion.WeightConversion(originalWeight, newWeightUnit);
+
+        convertedWeight.Value.Should().BeApproximately(convertedValue, 0.0001);
+        convertedWeight.Unit.Should().Be(newWeightUnit);
+    }
+}
