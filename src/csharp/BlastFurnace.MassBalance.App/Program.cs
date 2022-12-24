1e3b3434305ed93b9b45abb6e652599b94a73cc1
Dickson Souza <disouzam@yahoo.com.br>
2022-12-24 18:08:19 -0300

    Calling DescriptiveText property of an instance of header class and displayed in console

--- src/csharp/BlastFurnace.MassBalance.App/Program.cs
+++ src/csharp/BlastFurnace.MassBalance.App/Program.cs
@@ -6,6 +6,9 @@ static class Program
 {
     static void Main()
     {
+        var header = new Header();
         Console.WriteLine("Blast Furnace Mass Balance Application");
+
+        Console.WriteLine(header.DescriptiveText);
     }
 }
