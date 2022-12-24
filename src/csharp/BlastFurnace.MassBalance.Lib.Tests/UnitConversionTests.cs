e975e46ebdcc31877f7b0f8c59b83bf9637d5b47
Dickson Souza <disouzam@yahoo.com.br>
2022-12-24 16:44:06 -0300

    Fixed expected response and extended tests for UnitConversion
    - Fixed expected response for CheckValidConversions test method
    - Added more test cases to increase code coverage and cover all possible scenarios (in terms of branches)
    
    - And extending unit conversion tests to cover for non-existent enumeration:
    https://stackoverflow.com/questions/10603725/ncover-exclude-unexecutable-line-of-code-from-coverage
    (response from Sergey Berezovskiy, May 15, 2012 at 15:15)
    
    A copy of the beginning of his response follows below:
    
    You can exercise it by casting integer value, which is not exists in OperandType enumeration to OperandType:
    
    Assert.Throws<InvalidEnumArgumentException>(delegate { ((OperandType)Int32.MaxValue).ToShortName(); } );

--- src/csharp/BlastFurnace.MassBalance.Lib.Tests/UnitConversionTests.cs
+++ src/csharp/BlastFurnace.MassBalance.Lib.Tests/UnitConversionTests.cs
@@ -1,4 +1,7 @@
-﻿using FluentAssertions;
+﻿using System;
+using System.ComponentModel;
+
+using FluentAssertions;
 
 using Xunit;
 
@@ -8,7 +11,9 @@ public class UnitConversionTests
 {
     [Theory]
     [InlineData(1, WeightUnits.kilogram, 0.001, WeightUnits.metricTon)]
-    [InlineData(1, WeightUnits.metricTon, 999, WeightUnits.kilogram)]
+    [InlineData(1, WeightUnits.kilogram, 1, WeightUnits.kilogram)]
+    [InlineData(1, WeightUnits.metricTon, 1000, WeightUnits.kilogram)]
+    [InlineData(1, WeightUnits.metricTon, 1, WeightUnits.metricTon)]
     public void CheckValidConversions(double originalValue, WeightUnits originalWeightUnit, double convertedValue, WeightUnits newWeightUnit)
     {
         var originalWeight = new Weight(originalValue, originalWeightUnit);
@@ -17,5 +22,21 @@ public class UnitConversionTests
 
         convertedWeight.Value.Should().BeApproximately(convertedValue, 0.0001);
         convertedWeight.Unit.Should().Be(newWeightUnit);
+    }
+
+    [Fact]
+    public void CheckInvalidWeightConversions()
+    {
+        var originalWeight = new Weight(1, WeightUnits.kilogram);
+        var act = () => UnitConversion.WeightConversion(originalWeight, (WeightUnits)int.MaxValue);
+        act.Should().Throw<NotImplementedException>().WithMessage("Conversion between kilogram and 2147483647 is not implemented yet!");
+
+        var originalWeight2 = new Weight(1, WeightUnits.metricTon);
+        var act2 = () => UnitConversion.WeightConversion(originalWeight2, (WeightUnits)int.MaxValue);
+        act2.Should().Throw<NotImplementedException>().WithMessage("Conversion between metricTon and 2147483647 is not implemented yet!");
+
+        var originalWeight3 = new Weight(1, (WeightUnits)int.MaxValue);
+        var act3 = () => UnitConversion.WeightConversion(originalWeight3, WeightUnits.kilogram);
+        act3.Should().Throw<NotImplementedException>().WithMessage("Conversion between 2147483647 and kilogram is not implemented yet!");
     }
 }
