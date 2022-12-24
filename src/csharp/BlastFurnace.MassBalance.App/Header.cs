74bbc17d7686599a9e7602902f1f8aba34911a97
Dickson Souza <disouzam@yahoo.com.br>
2022-12-24 18:06:51 -0300

    Added Class Header to invoke About class and display text in console

---
+++ src/csharp/BlastFurnace.MassBalance.App/Header.cs
@@ -0,0 +1,16 @@
+﻿using BlastFurnace.MassBalance.Lib;
+
+namespace BlastFurnace.MassBalance.ConsoleApp
+{
+    internal class Header
+    {
+        private string descriptiveText = About.Description;
+
+        public string DescriptiveText { get { return descriptiveText; } }
+
+        public Header() 
+        { 
+
+        }
+    }
+}
