55b8722f61576eeb65101e9b3b393ab6cb0dd584
Dickson Souza <disouzam@yahoo.com.br>
2022-12-24 22:04:40 -0300

    Adjusted About class and introduced tests for checking strings in About class

--- src/csharp/BlastFurnace.MassBalance.Lib/About.cs
+++ src/csharp/BlastFurnace.MassBalance.Lib/About.cs
@@ -7,45 +7,43 @@ namespace BlastFurnace.MassBalance.Lib
     /// </summary>
     public static class About
     {
-        private static readonly string paragraph1 = "Este programa realiza um cálculo de carga simplificado para um alto forno," +
-                                                "feito com base nos exercícios feitos em sala de aula e, também, com base nos" +
-                                                "conhecimentos do grupo, tanto na montagem da linha de pensamento como na" +
+        private static readonly string paragraph1 = "Este programa realiza um cálculo de carga simplificado para um alto forno, " +
+                                                "feito com base nos exercícios feitos em sala de aula e, também, com base nos " +
+                                                "conhecimentos do grupo, tanto na montagem da linha de pensamento como na " +
                                                 "montagem do programa usando a linguagem Pascal.";
 
-        private static readonly string paragraph2 = "\n" + "O programa considera o carregamento de até 3 tipos de minérios" +
+        private static readonly string paragraph2 = "\r\n" + "O programa considera o carregamento de até 3 tipos de minérios " +
                                                 "(estes podem ser os minérios, pelotas e sínteres).";
 
-        private static readonly string paragraph3 = "\n" + "A partir de uma proporção em peso fixa, a carga de minérios é calculada." +
+        private static readonly string paragraph3 = "\r\n" + "A partir de uma proporção em peso fixa, a carga de minérios é calculada. " +
                                                 "Calcula-se também o teor médio de ferro contido nela.";
 
-        private static readonly string paragraph4 = "\n" + "A carga de coque é feita de forma similar: estipula-se a proporção dos" +
-                                                "diferentes tipos de coque(no máximo 3) e calcula-se a massa de cada um deles" +
+        private static readonly string paragraph4 = "\r\n" + "A carga de coque é feita de forma similar: estipula-se a proporção dos " +
+                                                "diferentes tipos de coque (no máximo 3) e calcula-se a massa de cada um deles" +
                                                 "para atender ao processo.";
 
-        private static readonly string paragraph5 = "\n" + "Pode-se calcular a quantidade de PCI máxima a se injetar, e a partir dela," +
+        private static readonly string paragraph5 = "\r\n" + "Pode-se calcular a quantidade de PCI máxima a se injetar, e a partir dela, " +
                                                 "estipular um valor a ser usado.";
 
-        private static readonly string paragraph6 = "\n" + "O sopro pode ser calculado, considerando-se a necessidade de oxigênio e" +
+        private static readonly string paragraph6 = "\r\n" + "O sopro pode ser calculado, considerando-se a necessidade de oxigênio e " +
                                                 "o seu teor no sopro.";
 
-        private static readonly string paragraph7 = "\n" + "Os cálculos são feitos com base nas considerações de que todo o ferro" +
-                                                "carregado é reduzido e se incorpora ao gusa líquido e a reação de redução da" +
-                                                "wustita a ferro se processam integralmente a 900 °C, de forma que a" +
+        private static readonly string paragraph7 = "\r\n" + "Os cálculos são feitos com base nas considerações de que todo o ferro " +
+                                                "carregado é reduzido e se incorpora ao gusa líquido e a reação de redução da " +
+                                                "wustita a ferro se processam integralmente a 900 °C, de forma que a " +
                                                 "reação de redução seja:";
 
-        private static readonly string paragraph8 = "\n" + "FeO +3,12CO = Fe + 2,12 CO + CO2.";
+        private static readonly string paragraph8 = "\r\n" + "FeO +3,12CO = Fe + 2,12 CO + CO2.";
 
-        private static readonly string paragraph9 = "\n" + "Após o uso do programa, pode-se armazenar os dados em um arquivo texto" +
+        private static readonly string paragraph9 = "\r\n" + "Após o uso do programa, pode-se armazenar os dados em um arquivo texto " +
                                                 "(Calculo_de_carga.doc)";
-
-        private static string description = string.Empty;
 
         private static string GetDescription()
         {
             var sb = new StringBuilder();
 
             sb.AppendLine(paragraph1);
-            sb.AppendLine(paragraph2);  
+            sb.AppendLine(paragraph2);
             sb.AppendLine(paragraph3);
             sb.AppendLine(paragraph4);
             sb.AppendLine(paragraph5);
@@ -54,19 +52,18 @@ namespace BlastFurnace.MassBalance.Lib
             sb.AppendLine(paragraph8);
             sb.AppendLine(paragraph9);
 
-            return sb.ToString();   
+            return sb.ToString();
         }
 
         /// <summary>
         /// A textual description of the purpose of Blast Furnace mass balance library/application
         /// </summary>
-        public static string Description 
-        { 
-            get 
-            { 
-                description = GetDescription();
-                return description; 
-            } 
+        public static string Description
+        {
+            get
+            {
+                return GetDescription();
+            }
         }
     }
 }
