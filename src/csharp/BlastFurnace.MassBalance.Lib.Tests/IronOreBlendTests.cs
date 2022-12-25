53ff2ca08f15cdd66f3d78bd9bb481b37d14aac0
Dickson Souza <disouzam@yahoo.com.br>
2022-12-25 17:44:43 -0300

    Added IronOreBlend class and related tests

---
+++ src/csharp/BlastFurnace.MassBalance.Lib.Tests/IronOreBlendTests.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+using FluentAssertions;
+
+using Xunit;
+
+namespace BlastFurnace.MassBalance.Lib.Tests;
+
+public class IronOreBlendTests
+{
+    [Fact]
+    public void CreateNewObject()
+    {
+        var obj = new IronOreBlend();
+        obj.Should().NotBeNull();
+    }
+
+    [Fact]
+    public void AddOneIronOreToAnEmptyIronOreBlend()
+    {
+        var obj = new IronOreBlend();
+        var ironOre = new IronOre(new Percentual(70), new Percentual(100));
+
+        var act = () => obj.Add(ironOre);
+
+        act.Should().NotThrow();
+        obj.Should().NotBeNull();
+    }
+
+    [Fact]
+    public void AddIronOresAndExceedHundredPercent()
+    {
+        var obj = new IronOreBlend();
+        var ironOre = new IronOre(new Percentual(70), new Percentual(80));
+        obj.Add(ironOre);
+
+        var ironOre2 = new IronOre(new Percentual(65), new Percentual(75));
+        var act = () => obj.Add(ironOre2);
+
+        act.Should().Throw<InvalidOperationException>().WithMessage("Total proportion must be at a maximum of 100%.");
+        obj.Should().NotBeNull();
+        obj.TotalProportion.Should().Be(80);
+    }
+
+    [Fact]
+    public void AddOneIronOreAndNormalizeProportions()
+    {
+        var obj = new IronOreBlend();
+        var ironOre = new IronOre(new Percentual(70), new Percentual(75.15));
+
+        obj.Add(ironOre);
+        obj.NormalizeProportions();
+        obj.TotalProportion.Should().Be(100);
+    }
+}
---
+++ src/csharp/BlastFurnace.MassBalance.Lib/IronOreBlend.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BlastFurnace.MassBalance.Lib;
+
+/// <summary>
+/// A collection of Iron Ores
+/// </summary>
+public class IronOreBlend
+{
+    /// <summary>
+    /// Total proportion of iron ores in this blend.
+    /// </summary>
+    /// <remarks>
+    /// For a full set up iron ore blend, this value must be equal to 100%
+    /// For a partially defined iron ore blend, this value can be lower than 100%
+    /// </remarks>
+    public double TotalProportion { get; private set; }
+
+    private readonly List<IronOre> ironOres= new List<IronOre>();
+
+    /// <summary>
+    /// Add one iron ore to the blend of iron ores
+    /// </summary>
+    /// <param name="ironOre"></param>
+    /// <exception cref="InvalidOperationException"></exception>
+    public void Add(IronOre ironOre)
+    {
+        if (TotalProportion + ironOre.Proportion.Value > 100)
+        {
+            throw new InvalidOperationException("Total proportion must be at a maximum of 100%.");
+        }
+
+        TotalProportion += ironOre.Proportion.Value;
+        ironOres.Add(ironOre);
+    }
+
+    /// <summary>
+    /// Normalize proportions of iron ores to 100%
+    /// </summary>
+    public void NormalizeProportions()
+    {
+        var tempTotalProportion= 0d;
+
+        foreach (var ironOre in ironOres)
+        {
+            ironOre.Proportion.Value = ironOre.Proportion.Value / TotalProportion * 100;
+            tempTotalProportion += ironOre.Proportion.Value;
+        }
+
+        TotalProportion = tempTotalProportion;
+    }
+}
