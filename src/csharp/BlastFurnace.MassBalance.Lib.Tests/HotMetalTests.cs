a58459562b5fe7283e09c5f47b263a3888e28cb2
Dickson Souza <disouzam@yahoo.com.br>
2022-12-25 09:43:26 -0300

    Added HotMetal class and corresponding tests

---
+++ src/csharp/BlastFurnace.MassBalance.Lib.Tests/HotMetalTests.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+using FluentAssertions;
+
+using Xunit;
+
+namespace BlastFurnace.MassBalance.Lib.Tests;
+
+public class HotMetalTests
+{
+    [Fact]
+    public void CheckValidInitialization()
+    {
+        var hotMetal = new HotMetal(new Weight(1, WeightUnits.kilogram), new Percentual(95), new Percentual(5));
+
+        hotMetal.Should().NotBeNull();
+        hotMetal.Weight.Value.Should().Be(1);
+        hotMetal.Weight.Unit.Should().Be(WeightUnits.kilogram);
+        hotMetal.FePercent.Value.Should().Be(95);
+        hotMetal.CPercent.Value.Should().Be(5);
+
+        var hotMetal2 = new HotMetal(new Weight(1, WeightUnits.metricTon), new Percentual(94), new Percentual(5));
+
+        hotMetal2.Should().NotBeNull();
+        hotMetal2.Weight.Value.Should().Be(1);
+        hotMetal2.Weight.Unit.Should().Be(WeightUnits.metricTon);
+        hotMetal2.FePercent.Value.Should().Be(94);
+        hotMetal2.CPercent.Value.Should().Be(5);
+    }
+
+    [Fact]
+    public void CheckInvalidInitialization()
+    {
+        HotMetal hotMetal;
+        var act = () => hotMetal = new HotMetal(new Weight(1, WeightUnits.kilogram), new Percentual(100), new Percentual(5));
+
+        act.Should().Throw<InvalidOperationException>().WithMessage("Composition should not exceed 100%! Current value is at 105 %.");
+    }
+}
---
+++ src/csharp/BlastFurnace.MassBalance.Lib/HotMetal.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace BlastFurnace.MassBalance.Lib;
+
+/// <summary>
+/// Describes hot metal weight and composition
+/// </summary>
+public class HotMetal
+{
+    /// <summary>
+    /// Weight of hot metal
+    /// </summary>
+    public Weight Weight { get; set; }
+
+    /// <summary>
+    /// Percentage of Iron in Hot Metal
+    /// </summary>
+    public Percentual FePercent { get; set; }
+
+    /// <summary>
+    /// Percentage of Carbon in Hot Metal
+    /// </summary>
+    public Percentual CPercent { get; set; }
+
+    /// <summary>
+    /// Initialize and validate hot metal setting
+    /// </summary>
+    /// <param name="weight"></param>
+    /// <param name="fePercent"></param>
+    /// <param name="cPercent"></param>
+    public HotMetal(Weight weight, Percentual fePercent, Percentual cPercent)
+    {
+        if (fePercent.Value + cPercent.Value > 100)
+        {
+            throw new InvalidOperationException($"Composition should not exceed 100%! Current value is at {fePercent.Value + cPercent.Value} %.");
+        }
+        Weight = weight;
+        FePercent = fePercent;
+        CPercent = cPercent;
+    }
+}
