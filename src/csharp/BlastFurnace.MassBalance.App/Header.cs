da479c6368fd59b42ba6b12bc6f41ea4fa087ca9
Dickson Souza <disouzam@yahoo.com.br>
2022-12-25 14:27:28 -0300

    Formatting output of Header properties - using StringBuilder to better formatted strings

--- src/csharp/BlastFurnace.MassBalance.App/Header.cs
+++ src/csharp/BlastFurnace.MassBalance.App/Header.cs
@@ -1,20 +1,45 @@
-﻿using BlastFurnace.MassBalance.Lib;
+﻿using System.Text;
 
-namespace BlastFurnace.MassBalance.ConsoleApp
+using BlastFurnace.MassBalance.Lib;
+
+namespace BlastFurnace.MassBalance.ConsoleApp;
+
+internal class Header
 {
-    internal class Header
+    private readonly string mainDivider = "===============================================================================";
+
+    private readonly string secondaryDivider = "_______________________________________________________________________________";
+
+    private readonly string descriptiveText = About.Description;
+
+    public string DescriptiveText
     {
-        private readonly string descriptiveText = About.Description;
-
-        public string DescriptiveText { get { return descriptiveText; } }
-
-        private readonly string appTitle = About.AppTitle;
-
-        public string AppTitle { get { return appTitle; } }
-
-        public Header() 
-        { 
-
+        get
+        {
+            var sb = new StringBuilder();
+            sb.AppendLine(secondaryDivider);
+            sb.AppendLine(descriptiveText);
+            sb.AppendLine(secondaryDivider);
+            return sb.ToString();
         }
+    }
+
+    private readonly string appTitle = About.AppTitle;
+
+    public string AppTitle
+    {
+        get
+        {
+            var sb = new StringBuilder();
+            sb.AppendLine(mainDivider);
+            sb.AppendLine(appTitle);
+            sb.AppendLine(mainDivider);
+            return sb.ToString();
+        }
+    }
+
+    public Header()
+    {
+
     }
 }
