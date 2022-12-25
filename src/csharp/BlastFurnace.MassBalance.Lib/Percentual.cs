48b4b332b86b1f6d48cbf4ae6e27887ae074aced
Dickson Souza <disouzam@yahoo.com.br>
2022-12-25 09:44:23 -0300

    Added Percentual class and corresponding tests

---
+++ src/csharp/BlastFurnace.MassBalance.Lib/Percentual.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace BlastFurnace.MassBalance.Lib;
+
+/// <summary>
+/// Describe percent quantities (from 0 to 100%) that describe raw material chemistry
+/// </summary>
+public class Percentual
+{
+    /// <summary>
+    /// Hold the percentage value
+    /// </summary>
+    public double Value { get; set; }
+
+    /// <summary>
+    /// Initialize and validate Percentual instance
+    /// </summary>
+    /// <param name="value"></param>
+    public Percentual(double value)
+    {
+        if (value < 0 || value > 100)
+        {
+            throw new ArgumentOutOfRangeException(nameof(value), $"Percentage value must belong to [0,100] interval. Current value ({value}) is invalid!");
+        }
+        Value = value;
+    }
+}
