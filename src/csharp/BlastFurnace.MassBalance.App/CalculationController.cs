f52314e4668b3cb5ccecf9ac3c86800589c749d7
Dickson Souza <disouzam@yahoo.com.br>
2022-12-25 09:45:43 -0300

    Added a CalculationController to actually run the calculation of blast furnace mass balance

---
+++ src/csharp/BlastFurnace.MassBalance.App/CalculationController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace BlastFurnace.MassBalance.ConsoleApp
+{
+    internal static class CalculationController
+    {
+        public static void Run()
+        {
+            var selectedChoice = Console.ReadLine();
+            while (selectedChoice?.ToLower() == "Y")
+            {
+
+                selectedChoice = Console.ReadLine();
+            }
+        }
+    }
+}
