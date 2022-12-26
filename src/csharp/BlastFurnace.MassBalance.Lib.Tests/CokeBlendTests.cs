7d3e599ba505a0642084e2acd74e89ce6f3d53df
Dickson Souza <disouzam@yahoo.com.br>
2022-12-26 13:35:31 -0300

    Implemented CokeBlend and related tests

---
+++ src/csharp/BlastFurnace.MassBalance.Lib.Tests/CokeBlendTests.cs
@@ -0,0 +1,71 @@
+﻿using System;
+
+using FluentAssertions;
+
+using Xunit;
+
+namespace BlastFurnace.MassBalance.Lib.Tests;
+
+public class CokeBlendTests
+{
+    [Fact]
+    public void CreateNewObject()
+    {
+        var obj = new CokeBlend();
+        obj.Should().NotBeNull();
+    }
+
+    [Fact]
+    public void AddOneCokeToAnEmptyCokeBlend()
+    {
+        var obj = new CokeBlend();
+        var coke = new Coke(new Percentual(95), new Percentual(100));
+
+        var act = () => obj.Add(coke);
+
+        act.Should().NotThrow();
+        obj.Should().NotBeNull();
+    }
+
+    [Fact]
+    public void AddCokesAndExceedHundredPercent()
+    {
+        var obj = new CokeBlend();
+        var coke = new Coke(new Percentual(95), new Percentual(80));
+        obj.Add(coke);
+
+        var coke2 = new Coke(new Percentual(85), new Percentual(75));
+        var act = () => obj.Add(coke2);
+
+        act.Should().Throw<InvalidOperationException>().WithMessage("Total proportion must be at a maximum of 100%.");
+        obj.Should().NotBeNull();
+        obj.TotalProportion.Should().Be(80);
+    }
+
+    [Fact]
+    public void AddOneCokeAndNormalizeProportions()
+    {
+        var obj = new CokeBlend();
+        var coke = new Coke(new Percentual(95), new Percentual(75.15));
+
+        obj.Add(coke);
+        obj.NormalizeProportions();
+        obj.TotalProportion.Should().Be(100);
+    }
+
+    [Fact]
+    public void CheckStringRepresentation()
+    {
+        var obj = new CokeBlend();
+        var coke = new Coke(new Percentual(95), new Percentual(25));
+        obj.Add(coke);
+
+        var coke2 = new Coke(new Percentual(85), new Percentual(35));
+        obj.Add(coke2);
+
+        var coke3 = new Coke(new Percentual(80), new Percentual(40));
+        obj.Add(coke3);
+
+        obj.ToString().Should().Be("{\r\n  \"Cokes\": [\r\n    {\r\n      \"CContent\": {\r\n        \"Value\": 95.0\r\n      },\r\n      \"Proportion\": {\r\n        \"Value\": 25.0\r\n      }\r\n    },\r\n    {\r\n      \"CContent\": {\r\n        \"Value\": 85.0\r\n      },\r\n      \"Proportion\": {\r\n        \"Value\": 35.0\r\n      }\r\n    },\r\n    {\r\n      \"CContent\": {\r\n        \"Value\": 80.0\r\n      },\r\n      \"Proportion\": {\r\n        \"Value\": 40.0\r\n      }\r\n    }\r\n  ],\r\n  \"TotalProportion\": 100.0\r\n}");
+    }
+}
---
+++ src/csharp/BlastFurnace.MassBalance.Lib/CokeBlend.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+
+using Newtonsoft.Json;
+
+namespace BlastFurnace.MassBalance.Lib;
+
+/// <summary>
+/// A collection of Cokes
+/// </summary>
+public class CokeBlend
+{
+    private readonly List<Coke> cokes = new List<Coke>();
+
+    /// <summary>
+    /// A read-only collection of Cokes
+    /// </summary>
+    public ReadOnlyCollection<Coke> Cokes
+    {
+        get
+        {
+            return cokes.AsReadOnly();
+        }
+    }
+
+    /// <summary>
+    /// Total proportion of Cokes in this blend.
+    /// </summary>
+    /// <remarks>
+    /// For a full set up coke blend, this value must be equal to 100%
+    /// For a partially defined coke blend, this value can be lower than 100%
+    /// </remarks>
+    public double TotalProportion { get; private set; }
+
+    /// <summary>
+    /// Add one coke to the blend of Cokes
+    /// </summary>
+    /// <param name="coke"></param>
+    /// <exception cref="InvalidOperationException"></exception>
+    public void Add(Coke coke)
+    {
+        if (TotalProportion + coke.Proportion.Value > 100)
+        {
+            throw new InvalidOperationException("Total proportion must be at a maximum of 100%.");
+        }
+
+        TotalProportion += coke.Proportion.Value;
+        cokes.Add(coke);
+    }
+
+    /// <summary>
+    /// Normalize proportions of Cokes to 100%
+    /// </summary>
+    public void NormalizeProportions()
+    {
+        var tempTotalProportion = 0d;
+
+        foreach (var coke in cokes)
+        {
+            coke.Proportion.Value = coke.Proportion.Value / TotalProportion * 100;
+            tempTotalProportion += coke.Proportion.Value;
+        }
+
+        TotalProportion = tempTotalProportion;
+    }
+
+    /// <summary>
+    /// String representation of CokeBlend
+    /// </summary>
+    /// <returns></returns>
+    public override string ToString()
+    {
+        var jsonRepresentation = JsonConvert.SerializeObject(this, Formatting.Indented);
+
+        return jsonRepresentation.ToString();
+    }
+}
