2d694e56c6274ced9e457ca59fa4928bada2f682
Dickson Souza <disouzam@yahoo.com.br>
2022-12-26 17:45:45 -0300

    Introduced AirBlow class and related tests

---
+++ src/csharp/BlastFurnace.MassBalance.Lib.Tests/AirBlowTests.cs
@@ -0,0 +1,22 @@
+﻿using FluentAssertions;
+
+using Xunit;
+
+namespace BlastFurnace.MassBalance.Lib.Tests;
+
+public class AirBlowTests
+{
+    [Fact]
+    public void CheckInitialization()
+    {
+        var airBlow = new AirBlow(new Percentual(21));
+        airBlow.O2Content.Value.Should().Be(21);
+    }
+
+    [Fact]
+    public void CheckStringRepresentation()
+    {
+        var airBlow = new AirBlow(new Percentual(21));
+        airBlow.ToString().Should().Be("{\r\n  \"O2Content\": {\r\n    \"Value\": 21.0\r\n  }\r\n}");
+    }
+}
