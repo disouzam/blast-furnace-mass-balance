598f6d37fad41bfabce55ebee7fa632222547c66
Dickson Souza <disouzam@yahoo.com.br>
2022-12-26 15:31:21 -0300

    Renamed local variables from IronOreBlendTests to improve readability

--- src/csharp/BlastFurnace.MassBalance.Lib.Tests/IronOreBlendTests.cs
+++ src/csharp/BlastFurnace.MassBalance.Lib.Tests/IronOreBlendTests.cs
@@ -11,61 +11,61 @@ public class IronOreBlendTests
     [Fact]
     public void CreateNewObject()
     {
-        var obj = new IronOreBlend();
-        obj.Should().NotBeNull();
+        var ironOreBlend = new IronOreBlend();
+        ironOreBlend.Should().NotBeNull();
     }
 
     [Fact]
     public void AddOneIronOreToAnEmptyIronOreBlend()
     {
-        var obj = new IronOreBlend();
+        var ironOreBlend = new IronOreBlend();
         var ironOre = new IronOre(new Percentual(70), new Percentual(100));
 
-        var act = () => obj.Add(ironOre);
+        var act = () => ironOreBlend.Add(ironOre);
 
         act.Should().NotThrow();
-        obj.Should().NotBeNull();
+        ironOreBlend.Should().NotBeNull();
     }
 
     [Fact]
     public void AddIronOresAndExceedHundredPercent()
     {
-        var obj = new IronOreBlend();
+        var ironOreBlend = new IronOreBlend();
         var ironOre = new IronOre(new Percentual(70), new Percentual(80));
-        obj.Add(ironOre);
+        ironOreBlend.Add(ironOre);
 
         var ironOre2 = new IronOre(new Percentual(65), new Percentual(75));
-        var act = () => obj.Add(ironOre2);
+        var act = () => ironOreBlend.Add(ironOre2);
 
         act.Should().Throw<InvalidOperationException>().WithMessage("Total proportion must be at a maximum of 100%.");
-        obj.Should().NotBeNull();
-        obj.TotalProportion.Should().Be(80);
+        ironOreBlend.Should().NotBeNull();
+        ironOreBlend.TotalProportion.Should().Be(80);
     }
 
     [Fact]
     public void AddOneIronOreAndNormalizeProportions()
     {
-        var obj = new IronOreBlend();
+        var ironOreBlend = new IronOreBlend();
         var ironOre = new IronOre(new Percentual(70), new Percentual(75.15));
 
-        obj.Add(ironOre);
-        obj.NormalizeProportions();
-        obj.TotalProportion.Should().Be(100);
+        ironOreBlend.Add(ironOre);
+        ironOreBlend.NormalizeProportions();
+        ironOreBlend.TotalProportion.Should().Be(100);
     }
 
     [Fact]
     public void CheckStringRepresentation()
     {
-        var obj = new IronOreBlend();
+        var ironOreBlend = new IronOreBlend();
         var ironOre = new IronOre(new Percentual(70), new Percentual(25));
-        obj.Add(ironOre);
+        ironOreBlend.Add(ironOre);
 
         var ironOre2 = new IronOre(new Percentual(65), new Percentual(35));
-        obj.Add(ironOre2);
+        ironOreBlend.Add(ironOre2);
 
         var ironOre3 = new IronOre(new Percentual(60), new Percentual(40));
-        obj.Add(ironOre3);
+        ironOreBlend.Add(ironOre3);
 
-        obj.ToString().Should().Be("{\r\n  \"IronOres\": [\r\n    {\r\n      \"FeContent\": {\r\n        \"Value\": 70.0\r\n      },\r\n      \"Proportion\": {\r\n        \"Value\": 25.0\r\n      }\r\n    },\r\n    {\r\n      \"FeContent\": {\r\n        \"Value\": 65.0\r\n      },\r\n      \"Proportion\": {\r\n        \"Value\": 35.0\r\n      }\r\n    },\r\n    {\r\n      \"FeContent\": {\r\n        \"Value\": 60.0\r\n      },\r\n      \"Proportion\": {\r\n        \"Value\": 40.0\r\n      }\r\n    }\r\n  ],\r\n  \"TotalProportion\": 100.0\r\n}");
+        ironOreBlend.ToString().Should().Be("{\r\n  \"IronOres\": [\r\n    {\r\n      \"FeContent\": {\r\n        \"Value\": 70.0\r\n      },\r\n      \"Proportion\": {\r\n        \"Value\": 25.0\r\n      }\r\n    },\r\n    {\r\n      \"FeContent\": {\r\n        \"Value\": 65.0\r\n      },\r\n      \"Proportion\": {\r\n        \"Value\": 35.0\r\n      }\r\n    },\r\n    {\r\n      \"FeContent\": {\r\n        \"Value\": 60.0\r\n      },\r\n      \"Proportion\": {\r\n        \"Value\": 40.0\r\n      }\r\n    }\r\n  ],\r\n  \"TotalProportion\": 100.0\r\n}");
     }
 }
