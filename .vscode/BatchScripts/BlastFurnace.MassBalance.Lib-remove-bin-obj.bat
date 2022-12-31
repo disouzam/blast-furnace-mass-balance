37376a4c6a325a891b764152c0f380ecae80063c
Dickson Souza <disouzam@yahoo.com.br>
2022-12-31 17:08:09 -0300

    Adjusted references and paths associated with MassBalance.Lib project

--- .vscode/BatchScripts/BlastFurnace.MassBalance.Lib-remove-bin-obj.bat
+++ .vscode/BatchScripts/BlastFurnace.MassBalance.Lib-remove-bin-obj.bat
@@ -9,12 +9,12 @@
 cd ..\
 md log
 cd ..\
-cd src\csharp\BlastFurnace.MassBalance.Lib
-dir /b /s > ..\..\.vscode\log\BlastFurnace.MassBalance.Lib-Files-0-before.txt
+cd csharp\src\BlastFurnace.MassBalance.Lib
+dir /b /s > ..\..\..\.vscode\log\BlastFurnace.MassBalance.Lib-Files-0-before.txt
 @echo on
 rd /S /Q bin
 rd /S /Q obj
 @echo off
-dir /b /s > ..\..\.vscode\log\BlastFurnace.MassBalance.Lib-Files-1-after.txt
+dir /b /s > ..\..\..\.vscode\log\BlastFurnace.MassBalance.Lib-Files-1-after.txt
 cd ..\
 @REM pause
