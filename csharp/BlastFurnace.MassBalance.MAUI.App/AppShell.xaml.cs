50cd522d39b147867e8793c6d3563d8b78702cea
Dickson Souza <disouzam@yahoo.com.br>
2023-01-01 22:57:21 -0300

    Added MAUI app to solution

---
+++ csharp/BlastFurnace.MassBalance.MAUI.App/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace BlastFurnace.MassBalance.MAUI.App
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
